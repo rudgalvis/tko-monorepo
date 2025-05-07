0305697db38139d213101eefe3719d7f39addfa3
Rokas Rudgalvis <rokas@rudgalvis.com>
2025-05-07 18:42:23 +0200

    chore: enable automatic discount endpoint

--- packages/shopify-nexus/src/routes/api/automatic-discount/[iso_country]/[variant_id]/+server.ts
+++ packages/shopify-nexus/src/routes/api/automatic-discount/[iso_country]/[variant_id]/+server.ts
@@ -1,18 +1,18 @@
-//import { error, json, type RequestHandler } from '@sveltejs/kit'
-//import { StorefrontApi } from 'api'
-//
-//const handler: RequestHandler = async ({ params }) => {
-//	const { variant_id, iso_country } = params
-//
-//	if (!variant_id) throw error(400, { message: 'Variant ID is required' })
-//	if (!iso_country) throw error(400, { message: 'Country code is required' })
-//
-//	const storefrontApi = new StorefrontApi()
-//
-//	return json({
-//		amount: await storefrontApi.getAutomaticDiscountForVariant(iso_country, +variant_id),
-//	})
-//}
-//
-//export const POST: RequestHandler = handler
-//export const GET: RequestHandler = handler
+import { error, json, type RequestHandler } from '@sveltejs/kit'
+import { StorefrontApi } from 'api'
+
+const handler: RequestHandler = async ({ params }) => {
+	const { variant_id, iso_country } = params
+
+	if (!variant_id) throw error(400, { message: 'Variant ID is required' })
+	if (!iso_country) throw error(400, { message: 'Country code is required' })
+
+	const storefrontApi = new StorefrontApi()
+
+	return json({
+		amount: await storefrontApi.getAutomaticDiscountForVariant(iso_country, +variant_id),
+	})
+}
+
+export const POST: RequestHandler = handler
+export const GET: RequestHandler = handler
