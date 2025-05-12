17dbe1191a7388f1bc06b7cc3b08bf693dffcaa6
Rokas Rudgalvis <rokas@rudgalvis.com>
2025-05-12 10:35:11 +0200

    fix: non defined markets showing wrong prices

--- packages/storefront-api/src/lib/nexus/NexusApi.ts
+++ packages/storefront-api/src/lib/nexus/NexusApi.ts
@@ -4,38 +4,37 @@ export class NexusApi {
 	private readonly BASE_URL = PUBLIC_NEXUS_BASE_URL;
 	private readonly API_VERSION_PATH = 'api';
 
-    private readonly API_ROUTES = {
+	private readonly API_ROUTES = {
 		GET_AUTOMATIC_DISCOUNT: (isoCode: string, variantId: number) =>
 			`automatic-discount/${isoCode}/${variantId}`,
-		GET_CURRENCY_RATES: (baseMarket: string) =>
-			`currency-rates/${baseMarket}`
+		GET_CURRENCY_RATES: (baseMarket: string) => `currency-rates/${baseMarket}`
 	};
 
-    async getAutomaticDiscount(market: string, variantId: number) {
-        const response = await fetch(
-            `${this.BASE_URL}/${this.API_VERSION_PATH}/${this.API_ROUTES.GET_AUTOMATIC_DISCOUNT(market, variantId)}`,
-            { method: 'GET' }
-        );
+	async getAutomaticDiscount(market: string, variantId: number) {
+		try {
+			const response = await fetch(
+				`${this.BASE_URL}/${this.API_VERSION_PATH}/${this.API_ROUTES.GET_AUTOMATIC_DISCOUNT(market, variantId)}`,
+				{ method: 'GET' }
+			);
 
-        try {
-            return await response.json();
-        } catch (e) {
-            console.error(e);
-        }
-    };
+			return await response.json();
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    async getCurrencyRates(market: string): Promise<Record<string, number> | null> {
-        const response = await fetch(
-            `${this.BASE_URL}/${this.API_VERSION_PATH}/${this.API_ROUTES.GET_CURRENCY_RATES(market)}`,
-            { method: 'GET' }
-        );
+	async getCurrencyRates(market: string): Promise<Record<string, number> | null> {
+		try {
+			const response = await fetch(
+				`${this.BASE_URL}/${this.API_VERSION_PATH}/${this.API_ROUTES.GET_CURRENCY_RATES(market)}`,
+				{ method: 'GET' }
+			);
 
-        try {
-            return await response.json();
-        } catch (e) {
-            console.error(e);
+			return await response.json();
+		} catch (e) {
+			console.error(e);
 
-            return null
-        }
-    };
+			return null;
+		}
+	}
 }
--- packages/ui/src/lib/store/currency.ts
+++ packages/ui/src/lib/store/currency.ts
@@ -1,6 +1,7 @@
 import { persistentWritable } from 'common-utils';
 import { NexusApi } from 'storefront-api';
 import { writable } from 'svelte/store';
+import availableCurrencies from '$lib/data/available-currencies.json' with { type: 'json' };
 
 const nexusApi = new NexusApi();
 
@@ -10,6 +11,10 @@ export const currencyRates = writable<Record<string, number> | null>(null);
 
 marketCurrency.subscribe(async (v) => {
 	if(!v) return
+
+	if(!availableCurrencies.find(e => e.currency === v)) {
+		return marketCurrency.set('EUR')
+	}
 
 	currencyRates.set(await nexusApi.getCurrencyRates(v));
 });
