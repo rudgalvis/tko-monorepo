32df29bcb7003e2230a052dc44dd72a43b89fe19
Rokas Rudgalvis <rokas@rudgalvis.com>
2025-11-05 13:02:38 +0200

    chore: merge prod into feat/post-launch-fixes

--- apps/shopify-theme/assets/global.js
+++ apps/shopify-theme/assets/global.js
@@ -732,7 +732,15 @@
   }
   
   customElements.define('variant-radios', VariantRadios);
+
   
+  const links = document.links; 
+  for (let i = 0, linksLength = links.length ; i < linksLength ; i++) { 
+    if (links[i].hostname !== window.location.hostname) { 
+      links[i].target = '_blank'; 
+      links[i].rel = 'noreferrer noopener'; 
+  } 
+}
   
 
   
